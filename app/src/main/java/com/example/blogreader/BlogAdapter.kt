9daaa7c36f0473c2249657b9cce1e8197de0c544
Agnes <agnesajema@gmail.com>
2024-06-29 19:38:03 +0300

    add changes

--- app/src/main/java/com/example/blogreader/BlogAdapter.kt
+++ app/src/main/java/com/example/blogreader/BlogAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.blogreader
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -8,25 +9,30 @@ import androidx.recyclerview.widget.RecyclerView
 
 class BlogAdapter (var blogList: List<Author>): RecyclerView.Adapter<BlogViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BlogViewHolder {
-        TODO("Not yet implemented")
+       val itemView = LayoutInflater.from(parent.context).inflate(R.layout.blog_card,parent,false)
+        return BlogViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: BlogViewHolder, position: Int) {
-        TODO("Not yet implemented")
+      var blog = blogList[position]
+        holder.tvTitle.text=blog.title
+        holder.tvLink.text=blog.link
+        holder.tvDate.text=blog.pubDate
+        holder.tvName.text=blog.name
+        holder.tvPreview.text=blog.articlePreview
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return blogList.size
     }
 }
 
 
 class BlogViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-    var tvAuthor = itemView.findViewById<TextView>(R.id.tvAvatar)
+
     var tvName = itemView.findViewById<TextView>(R.id.tvName)
     var tvDate = itemView.findViewById<TextView>(R.id.tvDate)
     var tvPreview = itemView.findViewById<TextView>(R.id.tvPreview)
-    var tvImage = itemView.findViewById<TextView>(R.id.tvImage)
-    var tvTile = itemView.findViewById<TextView>(R.id.tvBookTitle)
+    var tvTitle = itemView.findViewById<TextView>(R.id.tvBookTitle)
     var tvLink = itemView.findViewById<TextView>(R.id.tvLink)
 }
--- app/src/main/java/com/example/blogreader/MainActivity.kt
+++ app/src/main/java/com/example/blogreader/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.blogreader
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.inputmethod.InputBinding
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.blogreader.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tvNames)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvNames.layoutManager= LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        displayBlogs()
+
+    }
+    fun displayBlogs(){
+        val author1= Author("Agnes Ajema","Amazing Love","26 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        val author2= Author("Gifton Bandi","Amazing Love","27 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        val author3= Author("Precious Echenje","Amazing Love","28 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        val author4= Author("Ruth Bandi","Amazing Love","29 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        val author5= Author("Martha Phanice","Amazing Love","20 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        val author6= Author("Damaris Ayanga","Amazing Love","21 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        val author7= Author("Grace Achila","Amazing Love","22 June 2023","","Read more..","","The end gonna be sweet if we all believe \n" +
+                "she's an amazing soul to meet \n" )
+        var authors = listOf(author1,author2,author3,author4,author5,author6,author7)
+
+        val blogAdapter = BlogAdapter(authors)
+        binding.rvNames.adapter = blogAdapter
+
     }
 }
